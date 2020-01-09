99b577d34879a67a6828fce7fa50dcb716afa196
Gerrit Brands <gerrit.brands@portavita.nl>
2020-01-09 17:00:43 +0100

    BLZ 3 - initiele code

---
+++ src/BLZ_003.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+class Party{
+    public static void main(String[] args){
+        System.out.println("Opdracht van blz 3 uit het boek HeadFirst Java");
+        Frame f = new Frame();
+        Label l = new Label("Party at Tim's");
+        Button b = new Button ("You Bet");
+        Button c = new Button ("Shoot mne");
+        Panel p = new Panel();
+        p.add(l);
+    }
+}
+
