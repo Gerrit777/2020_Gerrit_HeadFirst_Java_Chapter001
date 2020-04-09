56d951d964827e5759fc687e67b29d491e5ef20f
Gerrit Brands <g.brands1@chello.nl>
2020-04-09 17:22:51 +0200

    PhraseOMatic -- chapter1 blz 16 oefening
    En
    
    party.class

---
+++ src/PhraseOMatic.java
@@ -0,0 +1,21 @@
+import javafx.scene.control.SliderBuilder;
+
+public class PhraseOMatic{
+
+    public static void main(String[] args){
+        String[] wordListOne = {"24/7","B-to-B","dynamic"};
+        String[] wordListTwo = {"empowered","oriented","shared"};
+        String[] wordListThree = {"mindshare","strategy","mission"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        int rand1 = (int) (Math.random()*oneLength);
+        int rand2 = (int) (Math.random()*twoLength);
+        int rand3 = (int) (Math.random()*threeLength);
+
+        String phrase = wordListOne[rand1] +" " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        System.out.println("What we need is a" + phrase);
+    }
+}
